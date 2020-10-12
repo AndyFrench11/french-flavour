56311d971df19ab73a5c2cdf67a44f53a17832b5
Andrew French <frerew101@hotmail.com>
2020-10-12 21:16:31 +1300

    Added template Blog files and set dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Blog from './blog/Blog';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
+
+  const theme = createMuiTheme({
+      palette: {
+        type: 'dark',
+      },
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          PENIS
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Blog />
+    </ThemeProvider>
   );
 }
 
 export default App;
+
